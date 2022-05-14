43c9da511fe0730f7d39494013c69cc3a7b4a4a7
abled007 <audreybledsoe3@gmail.com>
2022-05-14 09:36:32 -0700

    post model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    img = models.ImageField(max_length=500)
+    caption = models.TextField()
+
+    def __str__(self):
+        return self.title
