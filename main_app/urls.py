422c0c5def1ccbba0036f02f7fe6977eb06d881e
abled007 <audreybledsoe3@gmail.com>
2022-05-14 11:35:48 -0700

    post view

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
+    path('posts/', views.PostList.as_view(), name="post_list")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from .models import Post
 # Create your views here.
 
 class Home(TemplateView):
     template_name = 'home.html'
+
+class PostList(TemplateView):
+    template_name = 'postlist.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.all() # Here we are using the model to query the database for us.
+        return context
