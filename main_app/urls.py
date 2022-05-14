7d4c880d814282177e78f7507aac3767c5bc8023
abled007 <audreybledsoe3@gmail.com>
2022-05-14 12:08:44 -0700

    create post

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('posts/', views.PostList.as_view(), name="post_list")
+    path('posts/', views.PostList.as_view(), name="post_list"),
+    path('post/add', views.PostCreate.as_view(), name="post_create")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from .models import Post
+from django.views.generic.edit import CreateView
 # Create your views here.
 
 class Home(TemplateView):
     template_name = 'home.html'
+
+class PostCreate(CreateView):
+    model = Post
+    fields = '__all__'
+    template_name = "post_create.html"
+    success_url = "/posts"
 
 class PostList(TemplateView):
     template_name = 'postlist.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["posts"] = Post.objects.all() # Here we are using the model to query the database for us.
+        title = self.request.GET.get("title")
+        if title != None:
+            context["posts"] = Post.objects.filter(title__icontains=title)
+        else:
+            context["posts"] = Post.objects.all()
         return context
